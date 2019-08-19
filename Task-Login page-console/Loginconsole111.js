0e514a19fcaafeb16232b2dc93960f935966478c
Nalini <nalinic42@gmail.com>
2019-08-18 20:51:38 -0700

    changes
    changes

---
+++ Task-Login page-console/Loginconsole111.js
@@ -0,0 +1,34 @@
+
+function unamebox() {
+    alert(" Area to enter the username")
+    
+}
+console.log("Clicked")
+
+
+
+function uname() {
+    alert("Enter the user name");
+}
+console.log("clicked");
+
+
+
+function pname() {
+    alert(" Enter the password")
+
+}
+console.log("Clicked")
+
+
+
+function pnamebax() {
+    alert(" Area to enter the password");
+}
+console.info("Area to enter the password");
+
+
+function init() {
+    alert("Login");
+}
+console.log("clicked");
