d710a234d4aae87b61b0e00f61c1e55508baa257
Nalini <nalinic42@gmail.com>
2019-09-09 21:20:39 -0700

    class-object-task-Android-registration
    ....

---
+++ Android/ClassObject/app/src/main/java/com/example/classobject/Register.java
@@ -0,0 +1,16 @@
+package com.example.classobject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    String name,course;
+
+    public Register(String name, String course) {
+        this.name= name;
+        this.course= course;
+    }
+
+
+
+
+}
